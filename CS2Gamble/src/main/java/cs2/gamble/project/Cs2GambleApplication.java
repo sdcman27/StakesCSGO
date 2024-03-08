b0aae345f3ee833592e88cef9959fb957a29e273
sethc <sethc@DESKTOP-EL1ITLK>
2024-03-08 12:22:51 -0500

    Init

---
+++ CS2Gamble/src/main/java/cs2/gamble/project/Cs2GambleApplication.java
@@ -0,0 +1,13 @@
+package cs2.gamble.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs2GambleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs2GambleApplication.class, args);
+	}
+
+}
---
+++ CS2Gamble/src/test/java/cs2/gamble/project/Cs2GambleApplicationTests.java
@@ -0,0 +1,13 @@
+package cs2.gamble.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs2GambleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
